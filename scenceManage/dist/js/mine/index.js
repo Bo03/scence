fc242dd852187a65f0a1f379fc7733ba2d455b87
Bo03 <18747080800@163.com>
2020-04-07 11:01:55 +0800

    管理端前端

---
+++ scenceManage/dist/js/mine/index.js
@@ -0,0 +1,10 @@
+// function changeFrameHeight(){
+//     var ifm= document.getElementById("myiframe"); 
+//     ifm.height=document.documentElement.clientHeight;
+
+// }
+
+// window.οnresize=function(){  
+//      changeFrameHeight();  
+
+// } 
